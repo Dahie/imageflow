9e497a9e205e7183be43facae0073a38d3e739f0
Dahie <public@danielsenff.de>
2009-10-10 10:04:12 +0200

    beautified intendation

--- src/de/danielsenff/imageflow/utils/UrlCheck.java
+++ src/de/danielsenff/imageflow/utils/UrlCheck.java
@@ -9,39 +9,39 @@ import java.net.URL;
  */
 public class UrlCheck {
 
-public static boolean exists(final String URLName){
-	try {
-		HttpURLConnection.setFollowRedirects(false);
-		// note : you may also need
-		// HttpURLConnection.setInstanceFollowRedirects(false);
-    	final HttpURLConnection con =	(HttpURLConnection) new URL(URLName).openConnection();
-    	con.setRequestMethod("HEAD");
-    		return (con.getResponseCode() == HttpURLConnection.HTTP_OK);
+	public static boolean exists(final String URLName){
+		try {
+			HttpURLConnection.setFollowRedirects(false);
+			// note : you may also need
+			// HttpURLConnection.setInstanceFollowRedirects(false);
+			final HttpURLConnection con =	(HttpURLConnection) new URL(URLName).openConnection();
+			con.setRequestMethod("HEAD");
+			return (con.getResponseCode() == HttpURLConnection.HTTP_OK);
 		} catch (final Exception e) {
 			e.printStackTrace();
-       		return false;
+			return false;
 		}
 	}
 
-public static boolean existsFile(final String URLName){
-	try {
-	    HttpURLConnection.setFollowRedirects(false);
-	    // note : you may also need
-	    // HttpURLConnection.setInstanceFollowRedirects(false);
-	    final HttpURLConnection con = (HttpURLConnection) new URL(URLName).openConnection();
-	    con.setRequestMethod("HEAD");
-	    
-	    if ((con.getResponseCode() == HttpURLConnection.HTTP_OK) &&
-	    		con.getContentType().equals("text/html") == false ){
-	    	final String contentType = con.getContentType();
-	    	System.out.println("Contenttype: " + contentType);
-	    	return true;
-	  	} else {
-	  		return false;
-	  	}
-	} catch (final Exception e) {
-	       e.printStackTrace();
-	       return false;
-	       }
+	public static boolean existsFile(final String URLName){
+		try {
+			HttpURLConnection.setFollowRedirects(false);
+			// note : you may also need
+			// HttpURLConnection.setInstanceFollowRedirects(false);
+			final HttpURLConnection con = (HttpURLConnection) new URL(URLName).openConnection();
+			con.setRequestMethod("HEAD");
+
+			if ((con.getResponseCode() == HttpURLConnection.HTTP_OK) &&
+					con.getContentType().equals("text/html") == false ){
+				final String contentType = con.getContentType();
+				System.out.println("Contenttype: " + contentType);
+				return true;
+			} else {
+				return false;
+			}
+		} catch (final Exception e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 }
