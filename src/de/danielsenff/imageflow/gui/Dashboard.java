7e84ba6591585e5bce1c6f586d9f7a61deb2e402
Dahie <mail@danielsenff.de>
2011-03-01 08:18:39 +0100

    refactored mouselistener interface for Dashboard panels

--- src/de/danielsenff/imageflow/gui/Dashboard.java
+++ src/de/danielsenff/imageflow/gui/Dashboard.java
@@ -4,13 +4,13 @@ package de.danielsenff.imageflow.gui;
 import java.awt.Dimension;
 import java.awt.Point;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.util.HashMap;
 
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 
-import net.miginfocom.swing.MigLayout;
 import de.danielsenff.imageflow.controller.GraphController;
 import de.danielsenff.imageflow.controller.ParameterWidgetController;
 import de.danielsenff.imageflow.models.unit.UnitElement;
@@ -55,19 +55,8 @@ public class Dashboard extends JPanel {
 			unit.setWidget(dash);
 			dash.setBounds(30, 30, 250, 150);
 			dash.setBorder(BorderFactory.createTitledBorder(unit.getLabel()));
-			dash.addMouseMotionListener(new MouseMotionListener() {
-				
-				public void mouseMoved(MouseEvent e) {
-				}
-				
-				public void mouseDragged(MouseEvent e) {
-					int offsetX = e.getPoint().x - dash.getLocation().x;  
-					int offsetY = e.getPoint().y - dash.getLocation().y;
-					
-					//e.translatePoint(offsetX, offsetY);
-					dash.setLocation(new Point(e.getPoint()));
-				}
-			});
+			dash.addMouseListener(new DragListener(dash));
+			dash.addMouseMotionListener(new DragListener(dash));
 			this.add(dash);
 		}
 	}
@@ -87,4 +76,49 @@ public class Dashboard extends JPanel {
 		this.dashs.clear();
 	}
 	
+	class DragListener implements MouseListener, MouseMotionListener {
+
+		JPanel dash;
+		Point offset;
+		Point delta;
+		
+		public DragListener(JPanel dash) {
+			this.dash = dash;
+			this.delta = new Point(0, 0);
+		}
+
+		public void mouseClicked(MouseEvent e) {
+			
+		}
+
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void mousePressed(MouseEvent e) {
+			
+		}
+
+		public void mouseReleased(MouseEvent e) {}
+
+		public void mouseDragged(MouseEvent e) {
+			//e.translatePoint(offset.x, offset.y);
+			int offsetX = delta.x - e.getPoint().x;
+			int offsetY = delta.y - e.getPoint().y;
+			
+			System.out.println(e.getPoint());
+			e.translatePoint(offsetX, offsetY);
+			dash.setLocation(e.getPoint());
+			delta = e.getPoint();
+		}
+
+		public void mouseMoved(MouseEvent arg0) {}
+		
+	}
 }
